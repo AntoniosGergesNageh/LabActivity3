7485f4cc55ce3fb9aed9bff100e4b41b26f7e363
t00nny <129798251+t00nny@users.noreply.github.com>
2024-09-23 16:12:57 -0400

    New feature1 added

--- app/src/main/java/edu/temple/helloworld/MainActivity.kt
+++ app/src/main/java/edu/temple/helloworld/MainActivity.kt
@@ -8,27 +8,21 @@ import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
-    // Declare view properties
+    // Declare view properties - the first one is done for you
     lateinit var displayTextView: TextView
-    lateinit var nameEditText: EditText
-    lateinit var clickMeButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Initialize with views
+        // Initialize with views defined in Layout - the first one is done for you
         displayTextView = findViewById(R.id.displayTextView)
-        nameEditText = findViewById(R.id.nameEditText)
-        clickMeButton = findViewById(R.id.clickMeButton)
 
-        clickMeButton.setOnClickListener {
-            val name = nameEditText.text.toString()
-            if (name.isEmpty()) {
-                nameEditText.error = "Please enter your name"
-            } else {
-                displayTextView.text = "Hello, $name"
-            }
+
+        findViewById<Button>(R.id.clickMeButton).setOnClickListener {
+            displayTextView.text = "Hello, ${findViewById<EditText>(R.id.nameEditText).text}"
         }
+
+
     }
 }
